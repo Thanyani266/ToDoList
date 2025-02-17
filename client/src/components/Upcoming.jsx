35c2680550e35d3d557175eff82f6319d0b9bab9
Thanyani266 <83734015+Thanyani266@users.noreply.github.com>
2025-02-17 15:00:46 +0200

    Update Upcoming.jsx

--- client/src/components/Upcoming.jsx
+++ client/src/components/Upcoming.jsx
@@ -247,7 +247,7 @@ const Upcoming = ({isSidebarOpen}) => {
       <ModalOne show={showModalOne} onClose={handleCloseModalOne}>
         <MDBContainer style={{textAlign: 'start'}}>
         <h5 className="fw-bold text-center">Task: </h5>
-        <div className="fs4 border p-2 rounded mb-2"><span className="fw-bold text-muted">Title: </span>{task && task.title}</div>
+        <div className="fs4 border p-2 rounded mb-2"><span className="fw-bold text-muted">Title: </span>{task?.title || 'No Title'}</div>
         <div className="fs4 border p-2 rounded mb-2"><span className="fw-bold text-muted">Description: </span>{task && task.description}</div>
         <div className="fs4 border p-2 rounded mb-2"><span className="fw-bold text-muted">List: </span>{task && task.category}</div>
         <div className="fs4 border p-2 rounded mb-2"><span className="fw-bold text-muted">Due date: </span>{task && task.date}</div>
