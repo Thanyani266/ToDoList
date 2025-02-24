a466560d6602282b840b2be1456bd4e3f6729c67
Thanyani266 <83734015+Thanyani266@users.noreply.github.com>
2025-02-24 13:41:32 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 //import DataList from './components/DataList';
 import Today from './components/Today';
+import NewTaskForm from './components/NewTaskForm';
+import DataList from './components/DataList';
 
 const App = () => {
   const [currentTask, setCurrentTask] = useState(null);
@@ -15,6 +17,8 @@ const App = () => {
   return (
     <div className="App">
       <h1>Task Manager</h1>
+      <NewTaskForm  currentTask={currentTask} setCurrentTask={setCurrentTask} />
+      <DataList onEditTask={handleEditTask} />
       <Today onEditTask={handleEditTask}  currentTask={currentTask} setCurrentTask={setCurrentTask} showModal={showModal} setShowModal={setShowModal}/>
     </div>
   );
