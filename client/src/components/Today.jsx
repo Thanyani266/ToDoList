b90cbec53bd380c71cad61162859c29797e4d733
Thanyani266 <83734015+Thanyani266@users.noreply.github.com>
2025-02-25 11:05:09 +0200

    Update Today.jsx

--- client/src/components/Today.jsx
+++ client/src/components/Today.jsx
@@ -172,7 +172,7 @@ const Today = ({isSidebarOpen, onEditTask, currentTask, setCurrentTask, showModa
           <MDBIcon fas icon='trash' size='lg'/>
         </MDBBtn></>
       </MDBListGroupItem>
-      )) : <div>No task(s) for today</div>}
+      )) : <MDBListGroupItem className="text-center fw-bold text-warning bg-secondary bg-opacicity-25 my-auto">No task(s) for today</MDBListGroupItem>}
       <ModalOne show={showModalOne} onClose={handleCloseModalOne}>
       { selectedTask ? (
         <MDBContainer className="border p-3 rounded bg-light" key={selectedTask.id} style={{ textAlign: 'start' }}>
