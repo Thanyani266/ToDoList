17810b6d8f8249e5efd758850d901a87ae95818d
Thanyani266 <83734015+Thanyani266@users.noreply.github.com>
2025-02-26 13:55:50 +0200

    Update store.jsx

--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import dataReducer from '../redux/dataSlice';
-import notesReducer from '../redux/dataSlice';
+import notesReducer from '../redux/notesSlice';
 
 export const store = configureStore({
   reducer: {
